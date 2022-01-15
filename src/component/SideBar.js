ded946fff124324e0167f2d1089ec5360b34da8c
Arthur Picard <arthur.picard7@gmail.com>
2022-01-15 04:23:46 +0100

    Add: @material-ui/icons added but still issues with it

--- src/component/SideBar.js
+++ src/component/SideBar.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import { Avatar } from '@material-ui/core';
+import DonutLargeIcon from '../@material-ui/icons/DonutLargeIcon';
+import ChatIcon from '../@material-ui/icons/Chat';
+import MoreVertIcon from '../@material-ui/icons/MoreVertIcon';
 
 const SideBar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar__header">
-
+        <Avatar />
+       <div className="sidebar__headerRight">
+         <DonutLargeIcon />
+         <ChatIcon />
+         <MoreVertIcon />
+       </div>
       </div>
+
       <div className="sidebar__search">
 
       </div>
+
       <div className="sidebar__chats">
-        
+
       </div>
     </div>
   )
