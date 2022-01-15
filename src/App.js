baaf749aaf578232a8f5e82082bf88cb333d77bb
Arthur Picard <arthur.picard7@gmail.com>
2022-01-15 03:55:34 +0100

    Add: SideBar and Chart components created and added to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.scss';
+import SideBar from './component/SideBar';
+import Chat from './component/Chat';
 
 const App = () => {
   return (
     <div className="App">
       <h1>Whatsapp clone</h1>
+      <SideBar />
       <div className="app__body">
         <h2>Here it will be</h2>
+        <Chat />
       </div>
     </div>
 
