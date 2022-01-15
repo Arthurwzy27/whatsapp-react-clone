fadd0b24d2ff524065336dabad242674b62632d0
Arthur Picard <arthur.picard7@gmail.com>
2022-01-15 03:48:53 +0100

    Update: App scss updated

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import './App.scss';
 
 const App = () => {
   return (
-    <div className="app__body">
+    <div className="App">
       <h1>Whatsapp clone</h1>
+      <div className="app__body">
+        <h2>Here it will be</h2>
+      </div>
     </div>
+
   )
 }
 
