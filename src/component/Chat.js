9ae7785d1b4bfc6d49fb31b71056c75651e502cd
Arthur Picard <arthur.picard7@gmail.com>
2022-01-15 20:57:45 +0100

    Add: Buttons added (send and delete) to Chat

--- src/component/Chat.js
+++ src/component/Chat.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import Button from '@mui/material/Button';
+
 
 const Chat = () => {
   return (
     <div>
+      <div className="buttons">
+        <Button variant="outlined" startIcon={<DeleteIcon />}>
+          Delete
+        </Button>
+        <Button variant="contained" endIcon={<SendIcon />}>
+          Send
+        </Button>
+      </div>
     </div>
   )
 }
--- src/component/SideBar.js
+++ src/component/SideBar.js
@@ -1,24 +1,28 @@
 import React from 'react';
+import './Sidebar.scss';
 import Avatar from '@mui/material/Avatar';
 import DataSaverOnIcon from '@mui/icons-material/DataSaverOn';
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SearchIcon from '@mui/icons-material/Search';
 
 const SideBar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar__header">
         <Avatar />
-       <div className="sidebar__headerRight">
+      </div>
+
+      <div className="sidebar__headerRight">
+        <div className="sidebar__search">
+          <SearchIcon />
+          <p>Search...</p>
+        </div>
         <DataSaverOnIcon />
         <ChatBubbleOutlineIcon />
         <MoreVertIcon />
-       </div>
       </div>
 
-      <div className="sidebar__search">
-
-      </div>
 
       <div className="sidebar__chats">
 
